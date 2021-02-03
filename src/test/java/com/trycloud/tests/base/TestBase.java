05d3083ac85391cf45db6915e3afe10a7b9c9f01
Marwan Subhi <marwan.subhi4@gmail.com>
2021-02-03 10:42:11 -0500

    More change

--- src/test/java/com/trycloud/tests/base/TestBase.java
+++ src/test/java/com/trycloud/tests/base/TestBase.java
@@ -1,19 +1,11 @@
 package com.trycloud.tests.base;
-
-import com.trycloud.tests.Marwan.pages.FilePage;
-import com.trycloud.tests.Marwan.pages.LoginPage;
 import com.trycloud.utilities.BrowserUtils;
 import com.trycloud.utilities.ConfigurationReader;
 import com.trycloud.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public abstract class TestBase {
@@ -28,9 +20,9 @@ public abstract class TestBase {
 
     @AfterMethod
     public void tearDown() {
-//        BrowserUtils.sleep(5);
-//        driver.close();
-//        driver.quit();
+        BrowserUtils.sleep(5);
+        driver.close();
+        driver.quit();
     }
 
 
