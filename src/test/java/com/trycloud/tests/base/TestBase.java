4f23be7da39b0c49527f6818993d6de077831aea
Marwan Subhi <marwan.subhi4@gmail.com>
2021-02-03 10:43:06 -0500

    More change on master

--- src/test/java/com/trycloud/tests/base/TestBase.java
+++ src/test/java/com/trycloud/tests/base/TestBase.java
@@ -1,21 +1,15 @@
 package com.trycloud.tests.base;
-
 import com.trycloud.utilities.BrowserUtils;
 import com.trycloud.utilities.ConfigurationReader;
 import com.trycloud.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-public class TestBase {
+public abstract class TestBase {
     public static WebDriver driver;
-
     @BeforeMethod
     public void setup() throws IOException {
         driver = WebDriverFactory.getDriver("chrome");
@@ -30,5 +24,6 @@ public class TestBase {
         driver.close();
         driver.quit();
     }
+
 
 }
