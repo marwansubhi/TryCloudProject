473091394ffe15d690ff0fbf8307a0e3cbda3693
EllaKoch <ms.kosya555@gmail.com>
2021-02-04 17:04:04 -0500

    renamed class

--- src/test/java/com/trycloud/tests/Ella/TC_1_UserAbleToSearchFiles_Folders_Users.java
+++
@@ -1,4 +0,0 @@
-package com.trycloud.tests.Ella;
-
-public class TC_1_UserAbleToSearchFiles_Folders_Users {
-}
---
+++ src/test/java/com/trycloud/tests/Ella/TC_1_UserAbleToSearch_Files_Folders_Users.java
@@ -0,0 +1,7 @@
+package com.trycloud.tests.Ella;
+
+public class TC_1_UserAbleToSearch_Files_Folders_Users {
+
+
+
+}
