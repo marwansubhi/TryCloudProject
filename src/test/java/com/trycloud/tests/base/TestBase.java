15abb8fb1f3ef7e0ea45738076d91f11cdacef75
Marwan Subhi <marwan.subhi4@gmail.com>
2021-02-03 23:04:28 -0500

    more changes on test cases

--- src/test/java/com/trycloud/tests/base/TestBase.java
+++ src/test/java/com/trycloud/tests/base/TestBase.java
@@ -1,15 +1,21 @@
 package com.trycloud.tests.base;
+
 import com.trycloud.utilities.BrowserUtils;
 import com.trycloud.utilities.ConfigurationReader;
 import com.trycloud.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-public abstract class TestBase {
+public class TestBase {
     public static WebDriver driver;
+
     @BeforeMethod
     public void setup() throws IOException {
         driver = WebDriverFactory.getDriver(ConfigurationReader.getProperty("browser"));
@@ -24,6 +30,5 @@ public abstract class TestBase {
 //        driver.close();
 //        driver.quit();
     }
-
 
 }
