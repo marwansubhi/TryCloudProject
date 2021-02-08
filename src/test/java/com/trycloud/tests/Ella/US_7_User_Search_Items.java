0e666c20513bd208299605977914448b0a23dca1
EllaKoch <ms.kosya555@gmail.com>
2021-02-07 22:11:19 -0500

    changed the url to urlQA2

--- src/test/java/com/trycloud/tests/Ella/US_7_User_Search_Items.java
+++ src/test/java/com/trycloud/tests/Ella/US_7_User_Search_Items.java
@@ -21,7 +21,6 @@ public class US_7_User_Search_Items {
     @BeforeMethod
     public void setUp() {
         Driver.getDriver().get(ConfigurationReader.getProperty("urlQA2"));
-        // Driver.getDriver().get("http://qa2.trycloud.net");//delete once the environment is set up
         Driver.getDriver().manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         Driver.getDriver().findElement(By.xpath(loginBox)).sendKeys(ConfigurationReader.getProperty("username"));
         Driver.getDriver().findElement(By.xpath(passwordBox)).sendKeys(ConfigurationReader.getProperty("password"));
